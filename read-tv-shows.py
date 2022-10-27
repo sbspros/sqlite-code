1ff107138c13e9db911defbe06217f3e3a782ed1
Richard Chamberlain <59230129+sbspros@users.noreply.github.com>
2022-10-27 12:09:13 -0600

    Update read-tv-shows.py

--- read-tv-shows.py
+++ read-tv-shows.py
@@ -1,4 +1,5 @@
 from common.BaseClass import BaseClass
+from common.SqlConnection import SqlConnection
 from models.ShowList import ShowList
 import os
 import inspect
@@ -10,11 +11,11 @@ __version__ = "1.0"
 __maintainer__ = "Richard Chamberlain"
 __email__ = "richard@sbspros.ca"
 __status__ = "Dev"
-""" A ulitity to find and update plex media"""
 
 if __name__ == '__main__':
     bc=BaseClass('config.ini')
-    shows=ShowList(bc,'db/media.db')
+    conn=SqlConnection('db/media.db')
+    shows=ShowList(bc,conn)
     if !shows.is_created():
         show.create_veiw()
         print("View has been created")
