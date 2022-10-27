5a3751d4354e449b0f5597d45d64e00dc586e4fe
Richard Chamberlain <59230129+sbspros@users.noreply.github.com>
2022-10-27 11:58:08 -0600

    Create PlexSections.py

---
+++ models/PlexSections.py
@@ -0,0 +1,39 @@
+from common.BaseClass import BaseClass
+from table.ShowList import ShowList as tableShowList
+
+import traceback
+
+__author__ = "Richard Chamberlain"
+__copyright__ = "Copyright 2022"
+__license__ = "MIT"
+__version__ = "1.0"
+__maintainer__ = "Richard Chamberlain"
+__email__ = "richard@sbspros.ca"
+__status__ = "Dev"
+
+class PlexSections():
+  table_name='plex_sections'
+  def __init__(self,bc:BaseClass):
+    if self.is_create()!=1;
+      self.create_table()
+    
+  def is_created(self,conn):
+    return conn.execute("""SELECT EXISTS (
+    SELECT 
+        name
+    FROM 
+        sqlite_schema 
+    WHERE 
+        type='table' AND 
+        name='{table}'
+    );""".format(table=PlexSection.table_name))
+  
+  def create_table(self,conn,bc:BaseClass):
+    try:
+      self.create_base_tables()
+      sql_create_table = """ CREATE TABLE IF NOT EXISTS  {table} AS ...; """.format(table=ShowList.table_name)
+      conn.execute(sql_create_table)
+    except:
+      bc.log.error("\t"+":"+traceback.format_exc())
+      raise SQLCreateError
+      
